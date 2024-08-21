18fd8c333794494dbf1a796df9fede77a9365244
borgesdobrant <dobrantzamanda8@gmail.com>
2024-08-21 20:48:00 -0300

    atualizacao projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args){
 
+        /*
+        *
+        *  > MAIOR
+        *  < MENOR
+        *  == IGUAL
+        *  != DIFERENTE
+        *  String1.equals(String2) Compara se Texto é igual
+        *
+        * */
+
+        int idade = 20;
+        String nome = "Igor";
+
+        if (idade >= 21){
+            System.out.println("Maior que 21 anos");
+        }else{
+            System.out.println("Menor que 21 anos");
+        }
+
     }
 }
