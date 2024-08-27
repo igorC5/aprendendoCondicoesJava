67e7c594ff1fd12ddb761741cc53b21eb409ef8f
borgesdobrant <dobrantzamanda8@gmail.com>
2024-08-26 22:42:26 -0300

    atualizacao projeto java

--- src/ExemploCondicao2.java
+++ src/ExemploCondicao2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ExemploCondicao2 {
 
     //Quero add um desconto gradativo
@@ -7,7 +9,31 @@ public class ExemploCondicao2 {
 
     // valor Menor que 1000  - 0%
 
-    int valor;
-    double desconto;
-    
+    public static void main(String[] args) {
+
+        double valorBruto = 3000;
+        double desconto;
+
+        // && E
+        // || ou
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Informe o valor bruto:");
+        valorBruto = scan.nextDouble();
+
+
+        if (valorBruto >= 3000){
+            desconto = valorBruto * 0.3;
+        } else if (valorBruto >= 2000) {
+            desconto = valorBruto * 0.2;
+        } else if (valorBruto >= 1000) {
+            desconto = valorBruto * 0.1;
+        }else{
+            desconto = 0;
+        }
+
+        System.out.println("Valor Bruto é: " + valorBruto);
+        System.out.println("Valor do desconto é: " + desconto);
+        System.out.println("O valor liquido é: " + (valorBruto - desconto));
+    }
 }
