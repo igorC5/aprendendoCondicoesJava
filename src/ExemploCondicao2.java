9119a133e7aeaafdf0e05d49feda91878d0e330a
borgesdobrant <dobrantzamanda8@gmail.com>
2024-08-26 21:27:19 -0300

    atualizacao projeto java

---
+++ src/ExemploCondicao2.java
@@ -0,0 +1,13 @@
+public class ExemploCondicao2 {
+
+    //Quero add um desconto gradativo
+    // valor Maior que 1000  - 10%
+    // valor Maior que 2000  - 20%
+    // valor Maior que 3000  - 30%
+
+    // valor Menor que 1000  - 0%
+
+    int valor;
+    double desconto;
+    
+}
