c1ab8c51b3fd326cce26cedba0e33086656c3461
borgesdobrant <dobrantzamanda8@gmail.com>
2024-08-28 19:51:16 -0300

    atualizacao projeto java

---
+++ src/Atividade1.java
@@ -0,0 +1,14 @@
+public class Atividade1 {
+    public static void main(String[] args) {
+        //ATIVIDADE 1
+        System.out.println("atividade 1");
+
+        int idade = 20;
+
+        if (idade >= 18) {
+            System.out.println("É maior de idade");
+        }else {
+            System.out.println("É menor de idade");
+        }
+    }
+}
---
+++ src/ScannerEx.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ScannerEx {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String nome = "";
+        Integer idade = 0;
+        Double peso = 0.0;
+
+        System.out.println("Escreva seu nome: ");
+        nome = input.nextLine();
+
+        System.out.println("Escreva sua idade: ");
+        idade = input.nextInt();
+
+        System.out.println("Escreva sua peso: ");
+        peso = input.nextDouble();
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Peso: " + peso);
+
+    }
+}
